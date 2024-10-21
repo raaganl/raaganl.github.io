edd6205e4281ad5b3e9c410b9d92acf0490b508f
Ragan Lee <raganwlee@icloud.com>
2024-10-21 10:57:19 -0600

    fixed screen adjustments on back button, added painting gallery

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 let portfolioLinks = ["herd.html"]
+let artworkLinks = ["paintings.html","dnd.html"]
 
-let cardElements = document.querySelectorAll(".card");
-for(let i = 0; i < cardElements.length; i++){
-    cardElements[i].addEventListener('click', function(){
+let portfolioCards = document.querySelectorAll(".card");
+for(let i = 0; i < portfolioCards.length; i++){
+    portfolioCards[i].addEventListener('click', function(){
         window.location.href = portfolioLinks[i];
     })
 }
+let artworkCards = document.querySelectorAll(".card2");
+for(let i = 0; i < artworkCards.length; i++){
+    artworkCards[i].addEventListener('click', function(){
+        window.location.href = artworkLinks[i];
+    })
+}
+
